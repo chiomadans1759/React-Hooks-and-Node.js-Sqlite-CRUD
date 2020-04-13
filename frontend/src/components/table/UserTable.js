0569a01760292975934822c0c57584d14037be30
chiomadans1759 <oluchukwuokpala1759@gmail.com>
2020-04-13 11:18:26 +0100

    Pushing the app to github

---
+++ frontend/src/components/table/UserTable.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const UserTable = props => (
+  
+    <table className="responsive-table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                props.users.length > 0 ? (
+                    props.users.map (user => (
+
+                        <tr key={user.id}>
+                            <td>{user.name}</td>
+                            <td>{user.username}</td>
+                            <td className="center-align">
+                                <button
+                                    style={{marginRight: '10px'}}
+                                    className="waves-effect waves-light btn-small"
+                                    onClick={() => props.editRow(user)}>
+                                    edit
+                                </button>
+
+                                <button 
+                                    className="waves-effect waves-light btn-small red darken-4"
+                                    onClick={() => props.deleteUser(user.id)}>
+                                    delete
+                                </button>
+                            </td> 
+                        </tr>
+                        ))
+                    ) : (
+                        <tr>
+                            <td colSpan={3}>{props.users[0]} No Users</td>
+                        </tr>
+                    )
+            }          
+        </tbody>
+    </table>
+);
+    
+export default UserTable;
